0649ac742157d4196795ae2f650ecb61961780e8
devEMEL <dominichenry882@gmail.com>
2025-01-14 19:19:37 +0100

    ETH to MON

--- components/MintButton.tsx
+++ components/MintButton.tsx
@@ -226,7 +226,7 @@ export const MintButton: React.FC<MintButtonProps> = ({
             className="w-full bg-white text-black font-bold py-4 px-8 rounded-lg hover:bg-white/90 disabled:bg-white/50 disabled:cursor-not-allowed transition-colors flex justify-center"
         >
             
-            {address ? `Mint for ${weiToEther(String(collection.price))} ETH` : <ConnectButton />}
+            {address ? `Mint for ${weiToEther(String(collection.price))} MON` : <ConnectButton />}
         </button>
         </div>
         
