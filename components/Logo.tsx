dc4c7b9a3339d861f71b5f2a5b52c7e1244ae327
devEMEL <dominichenry882@gmail.com>
2025-02-09 11:09:55 +0100

    logo

--- components/Logo.tsx
+++ components/Logo.tsx
@@ -1,11 +1,13 @@
 import Link from 'next/link';
 import React from 'react';
-
+import emelverseLogo from "../app/public/assets/emelverse.jpeg";
+import Image from 'next/image';
 export const Logo: React.FC = () => {
     return (
         <Link href="/" className="flex items-center space-x-2">
             <div className="w-8 h-8 bg-white rounded-lg flex items-center justify-center">
-                <span className="text-black font-bold text-xl">ML</span>
+                {/* <span className="text-black font-bold text-xl">ML</span> */}
+                <Image src={emelverseLogo} width={50} height={50} alt='emelverse logo' className='w-8 h-8' />
             </div>
             <span className="text-white text-xl font-bold">emelverse</span>
         </Link>
