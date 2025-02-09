4f2c6f62860eb56f56e8a4c93f3e5b6b7e3b05a0
devEMEL <dominichenry882@gmail.com>
2025-02-09 11:34:30 +0100

    favicon

--- components/Logo.tsx
+++ components/Logo.tsx
@@ -5,9 +5,9 @@ import Image from 'next/image';
 export const Logo: React.FC = () => {
     return (
         <Link href="/" className="flex items-center space-x-2">
-            <div className="w-8 h-8 bg-white rounded-lg flex items-center justify-center">
+            <div className="bg-white rounded-lg flex items-center justify-center">
                 {/* <span className="text-black font-bold text-xl">ML</span> */}
-                <Image src={emelverseLogo} width={50} height={50} alt='emelverse logo' className='w-8 h-8' />
+                <Image src={emelverseLogo} width={50} height={50} alt='emelverse logo' className='w-16 h-12' />
             </div>
             <span className="text-white text-xl font-bold">emelverse</span>
         </Link>
